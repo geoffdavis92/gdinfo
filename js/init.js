e7f35d7a43ad8fa8a45f317b835638a96c1bcc2c
geoffdavis92 <geoffdavis92@gmail.com>
2015-04-13 14:35:56 -0500

    update /portfolioand styles

--- js/init.js
+++ js/init.js
@@ -11,3 +11,37 @@ function hover(d){			// d => 20
 function print(){
 	// Button to print .html page?
 }
+
+$('ul li.hv').each(function(){
+	var classes = $(this).attr('class');
+		classes = classes.split(" ");
+	$(this).hover(function(){
+		if(classes[1] === "orange"){
+			$(this).addClass('bg-orange');
+		} else if(classes[1] === "blue"){
+			$(this).addClass('bg-blue');
+		} else if(classes[1] === "maroon"){
+			$(this).addClass('bg-maroon');
+		} else if(classes[1] === "gold"){
+			$(this).addClass('bg-gold');
+		} else if(classes[1] === "green"){
+			$(this).addClass('bg-green');
+		}
+	},function(){
+		if(classes[1] === "orange"){
+			$(this).removeClass('bg-orange');
+		} else if(classes[1] === "blue"){
+			$(this).removeClass('bg-blue');
+		} else if(classes[1] === "maroon"){
+			$(this).removeClass('bg-maroon');
+		} else if(classes[1] === "gold"){
+			$(this).removeClass('bg-gold');
+		} else if(classes[1] === "green"){
+			$(this).removeClass('bg-green');
+		}
+	})
+});
+
+$('button.btn.back').click(function(){
+	baseURL = $('base').attr('href');
+});
